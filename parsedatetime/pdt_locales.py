c821e08ce2ce371bcf9e3bd705a0af503eb9a072
Chris Cho <chris@sevenrooms.com>
2014-04-08 16:11:40 -0400

    corrected misspelling of 'thirteen'

--- parsedatetime/pdt_locales.py
+++ parsedatetime/pdt_locales.py
@@ -82,7 +82,7 @@ class pdtLocale_base(object):
         # Used to parse expressions like "in 5 hours"
         self.numbers = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
                          'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
-                         'ten': 10, 'eleven': 11, 'twelve': 12, 'thirtheen': 13,
+                         'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13,
                          'fourteen': 14, 'fifteen': 15, 'sixteen': 16,
                          'seventeen': 17, 'eighteen': 18, 'nineteen': 19,
                          'twenty': 20 }
