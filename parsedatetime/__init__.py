bear/parsedatetime

commit 848deb47e254bf367bbba74c393947ab74a558d4

Added support for dotted meridians.

parsedatetime/__init__.py
@@ -227,7 +227,7 @@ class Calendar:
         self.weekdyFlag    = False  # monday/tuesday/...
         self.dateStdFlag   = False  # 07/21/06
         self.dateStrFlag   = False  # July 21st, 2006
-        self.timeStdFlag   = False  # 5:50 
+        self.timeStdFlag   = False  # 5:50
         self.meridianFlag  = False  # am/pm
         self.dayStrFlag    = False  # tomorrow/yesterday/today/..
         self.timeStrFlag   = False  # lunch/noon/breakfast/...
@@ -1648,7 +1648,7 @@ class Calendar:
             m = m % 12      # get remaining months
 
             if mi < 0:
-                y *= -1                 # otherwise negative mi will give future dates                                
+                y *= -1                 # otherwise negative mi will give future dates
                 mth = mth - m           # sub months from start month
                 if mth < 1:             # cross start-of-year?
                     y   -= 1            #   yes - decrement year
@@ -1931,7 +1931,7 @@ def _initSymbols(ptc):
     Initialize symbols and single character constants.
     """
       # build am and pm lists to contain
-      # original case, lowercase and first-char
+      # original case, lowercase, first-char and dotted
       # versions of the meridian text
 
     if len(ptc.locale.meridian) > 0:
@@ -1940,9 +1940,11 @@ def _initSymbols(ptc):
 
         if len(am) > 0:
             ptc.am.append(am[0])
+            ptc.am.append('{}.{}.'.format(am[0], am[1]))
             am = am.lower()
             ptc.am.append(am)
             ptc.am.append(am[0])
+            ptc.am.append('{}.{}.'.format(am[0], am[1]))
     else:
         am     = ''
         ptc.am = [ '', '' ]
@@ -1953,9 +1955,11 @@ def _initSymbols(ptc):
 
         if len(pm) > 0:
             ptc.pm.append(pm[0])
+            ptc.pm.append('{}.{}.'.format(pm[0], pm[1]))
             pm = pm.lower()
             ptc.pm.append(pm)
             ptc.pm.append(pm[0])
+            ptc.pm.append('{}.{}.'.format(pm[0], pm[1]))
     else:
         pm     = ''
         ptc.pm = [ '', '' ]
@@ -2066,7 +2070,7 @@ class Constants(object):
         # week +1                       F
 
         self.CurrentDOWParseStyle = False
-        
+
         if self.usePyICU:
             self.locale = pdtLocales['icu'](self.localeID)
 

parsedatetime/tests/TestSimpleDateTimes.py
@@ -58,14 +58,20 @@ class test(unittest.TestCase):
         start  = datetime.datetime(self.yr, self.mth, self.dy, self.hr, self.mn, self.sec).timetuple()
         target = datetime.datetime(self.yr, self.mth, self.dy, 23, 0, 0).timetuple()
 
-        self.assertTrue(_compareResults(self.cal.parse('11:00:00 PM', start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('11:00 PM',    start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('11 PM',       start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('11PM',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('2300',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('23:00',       start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('11p',         start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('11pm',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00:00 PM',   start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00 PM',      start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11 PM',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11PM',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('2300',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('23:00',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11p',           start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11pm',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00:00 P.M.', start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00 P.M.',    start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11 P.M.',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11P.M.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11p.m.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11 p.m.',       start), (target, 2)))
 
         target = datetime.datetime(self.yr, self.mth, self.dy, 11, 0, 0).timetuple()
 
@@ -77,6 +83,12 @@ class test(unittest.TestCase):
         self.assertTrue(_compareResults(self.cal.parse('11:00',       start), (target, 2)))
         self.assertTrue(_compareResults(self.cal.parse('11a',         start), (target, 2)))
         self.assertTrue(_compareResults(self.cal.parse('11am',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00:00 A.M.', start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11:00 A.M.',    start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11 A.M.',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11A.M.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11a.m.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('11 a.m.',       start), (target, 2)))
 
         target = datetime.datetime(self.yr, self.mth, self.dy, 7, 30, 0).timetuple()
 
@@ -221,29 +233,39 @@ class test(unittest.TestCase):
         start  = datetime.datetime(self.yr, self.mth, self.dy, self.hr, self.mn, self.sec).timetuple()
         target = datetime.datetime(self.yr, self.mth, self.dy, 0, 0, 0).timetuple()
 
-        self.assertTrue(_compareResults(self.cal.parse('midnight',    start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12:00:00 AM', start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12:00 AM',    start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12 AM',       start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12AM',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12am',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12a',         start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('0000',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('00:00',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('midnight',      start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00:00 AM',   start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00 AM',      start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12 AM',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12AM',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12am',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12a',           start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('0000',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('00:00',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00:00 A.M.', start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00 A.M.',    start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12 A.M.',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12A.M.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12a.m.',        start), (target, 2)))
 
     def testNoon(self):
         start  = datetime.datetime(self.yr, self.mth, self.dy, self.hr, self.mn, self.sec).timetuple()
         target = datetime.datetime(self.yr, self.mth, self.dy, 12, 0, 0).timetuple()
 
-        self.assertTrue(_compareResults(self.cal.parse('noon',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12:00:00 PM', start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12:00 PM',    start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12 PM',       start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12PM',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12pm',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12p',         start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('1200',        start), (target, 2)))
-        self.assertTrue(_compareResults(self.cal.parse('12:00',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('noon',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00:00 PM',   start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00 PM',      start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12 PM',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12PM',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12pm',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12p',           start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('1200',          start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00',         start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00:00 P.M.', start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12:00 P.M.',    start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12 P.M.',       start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12P.M.',        start), (target, 2)))
+        self.assertTrue(_compareResults(self.cal.parse('12p.m.',        start), (target, 2)))
 
     # def testMonths(self):
     #

commit d5883801e74511fffe09d170c0bd264eaa2ac4e6

Restore Python 2.6 compatibility
Commit 848deb47 broke support for 2.6 as it does not have support for
implicit positional arguments.

parsedatetime/__init__.py
@@ -1984,11 +1984,11 @@ def _initSymbols(ptc):
 
         if len(am) > 0:
             ptc.am.append(am[0])
-            ptc.am.append('{}.{}.'.format(am[0], am[1]))
+            ptc.am.append('{0}.{1}.'.format(am[0], am[1]))
             am = am.lower()
             ptc.am.append(am)
             ptc.am.append(am[0])
-            ptc.am.append('{}.{}.'.format(am[0], am[1]))
+            ptc.am.append('{0}.{1}.'.format(am[0], am[1]))
     else:
         am     = ''
         ptc.am = [ '', '' ]
@@ -1999,11 +1999,11 @@ def _initSymbols(ptc):
 
         if len(pm) > 0:
             ptc.pm.append(pm[0])
-            ptc.pm.append('{}.{}.'.format(pm[0], pm[1]))
+            ptc.pm.append('{0}.{1}.'.format(pm[0], pm[1]))
             pm = pm.lower()
             ptc.pm.append(pm)
             ptc.pm.append(pm[0])
-            ptc.pm.append('{}.{}.'.format(pm[0], pm[1]))
+            ptc.pm.append('{0}.{1}.'.format(pm[0], pm[1]))
     else:
         pm     = ''
         ptc.pm = [ '', '' ]