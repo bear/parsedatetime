9cdc2d42717c0f17f908c8ea7da88913e8b17c87
Leonardo Borges Avelino <lborgav@gmail.com>
2015-04-17 12:49:12 -0300

    Adding string for Brazilian portuguese (pt_BR) locale

--- parsedatetime/pdt_locales.py
+++ parsedatetime/pdt_locales.py
@@ -371,6 +371,53 @@ class pdtLocale_es(pdtLocale_base):
         self.dp_order = [ 'd', 'm', 'y' ]
 
 
+class pdtLocale_ptBR(pdtLocale_base):
+    """
+    pt_BR Locale
+
+    """
+    def __init__(self):
+        super( pdtLocale_ptBR, self ).__init__()
+
+        self.localeID     = 'pt_BR'   # don't use a unicode string
+        self.dateSep      = [ '/' ]
+        self.usesMeridian = False
+        self.uses24       = True
+
+        self.Weekdays      = [ 'segunda-feira', 'ter\xe7a-feira', 'quarta-feira',
+                               'quinta-feira', 'sexta-feira', 's\xe1bado', 'domingo',
+                             ]
+        self.shortWeekdays = [ 'seg', 'ter', 'qua',
+                               'qui', 'sex', 's\xe1b', 'dom',
+                             ]
+        self.Months        = [ 'janeiro', 'fevereiro', 'mar\xe7o',
+                               'abril', 'maio', 'junho',
+                               'julho', 'agosto', 'setembro',
+                               'outubro', 'novembro', 'dezembro'
+                             ]
+        self.shortMonths   = [ 'jan', 'fev', 'mar',
+                               'abr', 'mai', 'jun',
+                               'jul', 'ago', 'set',
+                               'out', 'nov', 'dez'
+                             ]
+        self.dateFormats['full']   = "EEEE, d' de 'MMMM' de 'yyyy"
+        self.dateFormats['long']   = "d' de 'MMMM' de 'yyyy"
+        self.dateFormats['medium'] = "dd-MM-yy"
+        self.dateFormats['short']  = "dd/MM/yyyy"
+
+        self.timeFormats['full']   = "HH'H'mm' 'ss z"
+        self.timeFormats['long']   = "HH:mm:ss z"
+        self.timeFormats['medium'] = "HH:mm:ss"
+        self.timeFormats['short']  = "HH:mm"
+
+        self.dp_order = [ 'd', 'm', 'y' ]
+
+        self.units['seconds'] = [ 'segundo', 'seg', 's']
+        self.units['minutes'] = [ 'minuto', 'min',  'm']
+        self.units['days']    = [ 'dia',  'dias',   'd']
+        self.units['months']  = [ 'm\xeas',     'meses']
+
+
 class pdtLocale_de(pdtLocale_base):
     """
     de_DE Locale constants
@@ -551,7 +598,7 @@ class pdtLocale_nl(pdtLocale_base):
         self.Modifiers['vorige']    = -1
         self.Modifiers['over']        =  2
         self.Modifiers['eind van']      =  0
-        
+
         #morgen/abermorgen does not work, see http://code.google.com/p/parsedatetime/issues/detail?id=19
         self.dayOffsets['morgen']        =  1
         self.dayOffsets['vandaag']         =  0
