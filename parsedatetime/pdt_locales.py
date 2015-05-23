0d94a4590d636614f082c2246e2be9ce760a4ccd
ipaterson <ipaterson@newstex.com>
2015-05-22 22:50:44 -0400

    Added "a" and "an" as aliases of "one"

--- parsedatetime/pdt_locales.py
+++ parsedatetime/pdt_locales.py
@@ -71,10 +71,10 @@ class pdtLocale_base(object):
         self.dp_order = [ 'm', 'd', 'y' ]
 
         # Used to parse expressions like "in 5 hours"
-        self.numbers = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                         'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
-                         'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13,
-                         'fourteen': 14, 'fifteen': 15, 'sixteen': 16,
+        self.numbers = { 'zero': 0, 'one': 1, 'a': 1, 'an': 1, 'two': 2, 'three': 3,
+                         'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+                         'nine': 9, 'ten': 10, 'eleven': 11, 'twelve': 12,
+                         'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16,
                          'seventeen': 17, 'eighteen': 18, 'nineteen': 19,
                          'twenty': 20 }
 
