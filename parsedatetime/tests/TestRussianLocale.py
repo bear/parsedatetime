d221763be976329609c4f8eff6b676f6df599ff2
Alexander Sapronov <sapronov.alexander92@gmail.com>
2015-09-18 11:40:32 +0600

    ADD: improve russian locale

--- parsedatetime/tests/TestRussianLocale.py
+++ parsedatetime/tests/TestRussianLocale.py
@@ -2,7 +2,7 @@
 """
 Test parsing of simple date and times using the Russian locale
 """
-
+from __future__ import unicode_literals
 import unittest
 import time
 import datetime
@@ -77,6 +77,22 @@ class test(unittest.TestCase):
 
         self.assertExpectedResult(self.cal.parse('25.8', start), (target, 1))
         self.assertExpectedResult(self.cal.parse('25.08', start), (target, 1))
+
+    def testDatesLang(self):
+        target = datetime.datetime(2006, 8, 25, 23, 5).timetuple()
+        self.assertExpectedResult(self.cal.parse('25 августа 2006 23:05'), (target, 3))
+        target = datetime.datetime(2006, 8, 25, self.hr, self.mn, self.sec).timetuple()
+        self.assertExpectedResult(self.cal.parse('25 августа 2006'), (target, 1))
+        self.assertEqual(self.cal.parse('23:05')[0][3], 23)
+        self.assertEqual(self.cal.parse('23:05')[0][4], 5)
+
+    def testConjugate(self):
+        target = datetime.datetime(2006, 9, 25, 23, 5).timetuple()
+        self.assertExpectedResult(self.cal.parse('25 сентября 2006 23:05'), (target, 3))
+        #self.assertExpectedResult(self.cal.parse('25 сентябрь 2006 23:05'), (target, 3))
+
+
+
 
     # does not work with travis
     # datetime.now() return non correct data
