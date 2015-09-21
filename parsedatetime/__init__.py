c10ab61681629feba2533d758a5e031325135bb3
Mike Taylor <bear42@gmail.com>
2015-09-20 22:57:14 -0400

    bump version to v2 and add notes to CHANGES file

--- parsedatetime/__init__.py
+++ parsedatetime/__init__.py
@@ -28,7 +28,7 @@ from __future__ import with_statement, absolute_import
 __author__ = 'Mike Taylor (bear@bear.im)'
 __copyright__ = 'Copyright (c) 2004 Mike Taylor'
 __license__ = 'Apache v2.0'
-__version__ = '1.6'
+__version__ = '2.0'
 __contributors__ = ['Darshana Chhajed',
                     'Michael Lim (lim.ck.michael@gmail.com)',
                     'Bernd Zeimetz (bzed@debian.org)']
