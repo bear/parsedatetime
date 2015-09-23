cdd1f3410b8ae304485f7992ac6048e1277cffe1
Alexander Sapronov <sapronov.alexander92@gmail.com>
2015-09-23 09:22:57 +0600

    Add local locale from file

--- parsedatetime/pdt_locales/__init__.py
+++ parsedatetime/pdt_locales/__init__.py
@@ -5,11 +5,14 @@ pdt_locales
 
 All of the included locale classes shipped with pdt.
 """
+import os
 
 try:
     import PyICU as pyicu
 except:
     pyicu = None
+
+import yaml
 
 
 def lcase(x):
@@ -25,3 +28,46 @@ from .es import *
 from .nl_NL import *
 from .pt_BR import *
 from .ru_RU import *
+
+pdtLocales = [
+    'icu',
+    'en_US',
+    'en_AU',
+    'es_ES',
+    'de_DE',
+    'nl_NL',
+    'ru_RU',
+]
+
+
+def load_yaml(path):
+    """
+    Read yaml data from filepath
+    :param path:
+    :return:
+    """
+    with open(path, 'r') as fio:
+        return yaml.load(fio.read())
+
+
+def _get_yaml_path(locale):
+    """
+    Return filepath of locale file
+    :param locale:
+    :return:
+    """
+    return os.path.join(os.path.dirname(__file__), '%s.yaml' % locale)
+
+
+def load_locale(locale):
+    """
+    Return data of locale
+    :param locale:
+    :return:
+    """
+    assert locale in pdtLocales, "The locale '%s' is not supported" % locale
+    _data_base = load_yaml(_get_yaml_path('base'))
+    return _data_base.update(**load_yaml(_get_yaml_path(locale)))
+
+
+load_locale('ru_RU')
