73f1080484e05a2a3370ca46fae4ff5ac680c5fd
ipaterson <ipaterson@newstex.com>
2016-11-22 21:13:08 -0500

    Ported TestPhrases

---
+++ tests/test_phrases.py
@@ -0,0 +1,19 @@
+from .fixtures import pdtFixture
+
+
+@pdtFixture('phrases.yml')
+def test_phrases(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('phrases.yml')
+def test_end_of_phrases(calendar, phrase, sourceTime, target, context,
+                        assertLazyStructTimes):
+    result = calendar.parse(phrase, sourceTime)
+    assertLazyStructTimes(result[0], target.timetuple())
+    assert result[1] == context
+
+
+@pdtFixture('phrases.yml')
+def test_last_phrases(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
