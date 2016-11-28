20260c90aabdac39938f576fdd9b4fb5c7deed96
ipaterson <ipaterson@newstex.com>
2016-11-27 21:10:58 -0500

    Completed port of TestSimpleOffsets

--- tests/test_simple_offsets.py
+++ tests/test_simple_offsets.py
@@ -17,3 +17,49 @@ def test_offset_from_day_of_week_matching_source_time(calendar, phrase,
                                                       sourceTime, target,
                                                       context):
     assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_minutes_from_now(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_minutes_before_now(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_week_from_now(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_week_before_now(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_next_month(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_next_weekday(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_next_weekday_matching_source_time(calendar, phrase, sourceTime,
+                                           target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_next_weekday_with_time(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets.yml')
+def test_specials(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
