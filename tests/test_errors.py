c1cfd8b5b5c4ec7acdd22dcc6cb223f143488cac
ipaterson <ipaterson@newstex.com>
2017-02-27 08:54:30 -0500

    Ported TestErrors

---
+++ tests/test_errors.py
@@ -0,0 +1,31 @@
+from .fixtures import pdtFixture
+
+
+@pdtFixture('errors.yml')
+def test_out_of_range(calendar, phrase, sourceTime, context):
+    assert calendar.parse(phrase, sourceTime) == (sourceTime.timetuple(),
+                                                  context)
+
+
+@pdtFixture('errors.yml')
+def test_plain_numbers(calendar, phrase, sourceTime, context):
+    assert calendar.parse(phrase, sourceTime) == (sourceTime.timetuple(),
+                                                  context)
+
+
+@pdtFixture('errors.yml')
+def test_substring_phrases(calendar, phrase, sourceTime, context):
+    assert calendar.parse(phrase, sourceTime) == (sourceTime.timetuple(),
+                                                  context)
+
+
+@pdtFixture('errors.yml')
+def test_substring_dates(calendar, phrase, sourceTime, context):
+    assert calendar.parse(phrase, sourceTime) == (sourceTime.timetuple(),
+                                                  context)
+
+
+@pdtFixture('errors.yml')
+def test_overflow(calendar, phrase, sourceTime, context):
+    assert calendar.parse(phrase, sourceTime) == (sourceTime.timetuple(),
+                                                  context)
