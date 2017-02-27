290831a6108122484238184b037eff3f8046eabe
ipaterson <ipaterson@newstex.com>
2017-02-26 20:52:23 -0500

    Added midnight tests for day_start_hour

--- tests/test_day_start_hour.py
+++ tests/test_day_start_hour.py
@@ -7,6 +7,11 @@ def test_default_day_start_hour(calendar, phrase, sourceTime, target, context):
 
 
 @pdtFixture('day_start_hour.yml')
+def test_midnight_day_start_hour(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('day_start_hour.yml')
 def test_morning_day_start_hour(calendar, phrase, sourceTime, target, context):
     assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
 
