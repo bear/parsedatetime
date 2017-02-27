36fcf9ea40486a8a38cf009792bb2ff379580692
ipaterson <ipaterson@newstex.com>
2017-02-26 20:40:07 -0500

    Ported TestDayStartHour

---
+++ tests/test_day_start_hour.py
@@ -0,0 +1,16 @@
+from .fixtures import pdtFixture
+
+
+@pdtFixture('day_start_hour.yml')
+def test_default_day_start_hour(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('day_start_hour.yml')
+def test_morning_day_start_hour(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('day_start_hour.yml')
+def test_evening_day_start_hour(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
