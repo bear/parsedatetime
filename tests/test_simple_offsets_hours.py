67b52a8303a14ef1f841e02b7e944daf55bc01ce
ipaterson <ipaterson@newstex.com>
2017-02-27 19:34:39 -0500

    Ported TestSimpleOffsetsNoon

--- tests/test_simple_offsets_hours.py
+++ tests/test_simple_offsets_hours.py
@@ -15,3 +15,12 @@ def test_invalid_hours_from_now(calendar, phrase, sourceTime, target, context):
 @pdtFixture('simple_offsets_hours.yml')
 def test_hours_before_now(calendar, phrase, sourceTime, target, context):
     assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+
+@pdtFixture('simple_offsets_hours.yml')
+def test_hours_from_noon(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
+
+@pdtFixture('simple_offsets_hours.yml')
+def test_hours_before_noon(calendar, phrase, sourceTime, target, context):
+    assert calendar.parse(phrase, sourceTime) == (target.timetuple(), context)
