c78178d735a828d912bf6ee5d9cb9041ddf3c9bc
ipaterson <ipaterson@newstex.com>
2017-03-08 20:44:57 -0500

    Treat skipped tests as errors and note when the skip may be due to a missing test group or file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,21 @@
 Preloads fixtures and adds any test hooks or plugins.
 """
 
+import pytest
+
 from tests.lib.assertions import *
 from tests.lib.fixtures import *
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.skipped and call.excinfo.errisinstance(pytest.skip.Exception):
+        rep.outcome = 'failed'
+        r = call.excinfo._getreprcrash()
+        message = 'Invalid test configuration'
+        if 'empty parameter set' in r.message:
+            message = 'No data found for test group'
+        rep.longrepr = '%s - %s' % (message, r.message)
