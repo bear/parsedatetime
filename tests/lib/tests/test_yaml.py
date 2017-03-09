108b19fb4602ec049c4e4c28f9112d17c40ca3f3
ipaterson <ipaterson@newstex.com>
2017-03-08 20:46:00 -0500

    Added some metatests to achieve full coverage of the test lib

---
+++ tests/lib/tests/test_yaml.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+from yaml import YAMLError
+
+from parsedatetime.context import pdtContext
+from tests.lib.data import datedelta, dateReplacement, loadData, pdtContext, \
+    nlpTarget
+
+
+@pytest.fixture
+def basedir():
+    return os.path.dirname(__file__)
+
+
+@pytest.fixture
+def constructorData(basedir):
+    return loadData(os.path.join(basedir, 'data/en_US/constructors.yml'))
+
+
+def test_yaml_error(basedir):
+    with pytest.raises(YAMLError):
+        loadData(os.path.join(basedir, 'data/en_US/yaml_error.yml'))
+
+
+def test_datedelta_constructor(constructorData):
+    constructorData['datedelta'] == datedelta(years=1, days=1)
+
+
+def test_dateReplacement_constructor(constructorData):
+    constructorData['dateReplacement'] == dateReplacement(month=1, hour=1)
+    
+
+def test_pdtContext_constructor(constructorData):
+    flags = pdtContext.ACU_YEAR | pdtContext.ACU_HOUR | pdtContext.ACU_SEC
+    constructorData['pdtContext'] == pdtContext(flags)
+    
