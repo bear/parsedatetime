49068d02ca164638c67fbcebd739d763a9bd2c8a
Caio Alexandre <contato.kaiyo@gmail.com>
2020-11-05 15:12:04 -0300

    Add more words in pt_BR vocabulary

--- parsedatetime/pdt_locales/pt_BR.py
+++ parsedatetime/pdt_locales/pt_BR.py
@@ -10,8 +10,8 @@ uses24 = True
 decimal_mark = ','
 
 Weekdays = [
-    'segunda-feira', 'terça-feira', 'quarta-feira',
-    'quinta-feira', 'sexta-feira', 'sábado', 'domingo',
+    'segunda|segunda-feira', 'terça|terça-feira', 'quarta|quarta-feira',
+    'quinta|quinta-feira', 'sexta|sexta-feira', 'sábado', 'domingo',
 ]
 shortWeekdays = [
     'seg', 'ter', 'qua', 'qui', 'sex', 'sáb', 'dom',
@@ -40,9 +40,118 @@ timeFormats = {
 
 dp_order = ['d', 'm', 'y']
 
+numbers = {
+    'zero': 0,
+    'um': 1,
+    'um(a)': 1,
+    'um(a)': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10,
+    'onze': 11,
+    'treze': 13,
+    'quatorze': 14,
+    'quinze': 15,
+    'dezesseis': 16,
+    'dezessete': 17,
+    'dezoito': 18,
+    'dezenove': 19,
+    'vinte': 20,
+}
+
 units = {
     'seconds': ['segundo', 'seg', 's'],
     'minutes': ['minuto', 'min', 'm'],
     'days': ['dia', 'dias', 'd'],
     'months': ['mês', 'meses'],
+}
+
+Modifiers = {
+    'de': 1,
+    'antes': -1,
+    'depois': 1,
+    'atrás': -1,
+    'anterior': -1,
+    'anterior': -1,
+    'último': -1,
+    'próximo': 1,
+    'anterior': -1,
+    'fim do': 0,
+    'esta': 0,
+    'fim do dia': 1,
+    'fim do mês': 1,
+    'fim do ano': 1,
+}
+
+dayOffsets = {
+    'amanhã': 1,
+    'hoje': 0,
+    'ontem': -1,
+}
+
+re_sources = {
+    'meio dia': {'hr': 12, 'mn': 0, 'sec': 0},
+    'tarde': {'hr': 13, 'mn': 0, 'sec': 0},
+    'almoço': {'hr': 12, 'mn': 0, 'sec': 0},
+    'manhã': {'hr': 6, 'mn': 0, 'sec': 0},
+    'café da manhã': {'hr': 8, 'mn': 0, 'sec': 0},
+    'jantar': {'hr': 19, 'mn': 0, 'sec': 0},
+    'tarde': {'hr': 18, 'mn': 0, 'sec': 0},
+    'meia noite': {'hr': 0, 'mn': 0, 'sec': 0},
+    'noite': {'hr': 21, 'mn': 0, 'sec': 0},
+    'esta noite': {'hr': 21, 'mn': 0, 'sec': 0},
+    'final do dia': {'hr': 17, 'mn': 0, 'sec': 0},
+}
+
+small = {
+    'zero': 0,
+    'um': 1,
+    'um(a)': 1,
+    'um(a)': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10,
+    'onze': 11,
+    'doze': 12,
+    'treze': 13,
+    'quatorze': 14,
+    'quinze': 15,
+    'dezesseis': 16,
+    'dezessete': 17,
+    'dezoito': 18,
+    'dezenove': 19,
+    'vinte': 20,
+    'trinta': 30,
+    'quarenta': 40,
+    'cinquenta': 50,
+    'sessenta': 60,
+    'setenta': 70,
+    'oitenta': 80,
+    'noventa': 90
+}
+
+magnitude = {
+    'mil': 1000,
+    'milhão': 1000000,
+    'bilhão': 1000000000,
+    'trilhão': 1000000000000,
+    'quadrilhão': 1000000000000000,
+    'quintilhão': 1000000000000000000,
+    'sextilhão': 1000000000000000000000,
+    'septilhão': 1000000000000000000000000,
+    'octilhão': 1000000000000000000000000000,
+    'nonilhão': 1000000000000000000000000000000,
+    'decilhão': 1000000000000000000000000000000000,
 }
