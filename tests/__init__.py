e40cad16e47370f2231eb8bf0f57267c9cdbfb81
Mike Taylor <bear@circleci.com>
2021-10-10 15:47:10 -0400

    Logging in Python 3 is a bit different, especially within a module Strip out all explicit logging setup calls

--- tests/__init__.py
+++ tests/__init__.py
@@ -18,11 +18,3 @@ __contributors__ = ['Darshana Chhajed',
                     'Michael Lim (lim.ck.michael@gmail.com)',
                     'Bernd Zeimetz (bzed@debian.org)',
                     ]
-
-
-log = logging.getLogger('parsedatetime')
-echoHandler = logging.StreamHandler()
-echoFormatter = logging.Formatter('%(levelname)-8s %(message)s')
-log.addHandler(echoHandler)
-
-# log.setLevel(logging.DEBUG)
